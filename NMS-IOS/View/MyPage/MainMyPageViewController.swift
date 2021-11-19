f59c8808c27895c6b2b2777e823c0e4a4d7338d4
김대희 <0824dh@naver.com>
2021-11-19 23:50:07 +0900

    📑 :: MainMyPageViewController

---
+++ NMS-IOS/View/MyPage/MainMyPageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainMyPageViewController.swift
+//  NMS-IOS
+//
+//  Created by 김대희 on 2021/11/19.
+//
+
+import UIKit
+
+class MainMyPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+}
