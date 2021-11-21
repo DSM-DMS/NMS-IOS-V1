75cb7495ae918dbf074329fd856d3c5d8346bbe3
김대희 <0824dh@naver.com>
2021-11-22 08:35:17 +0900

    📑 :: CustomCollectionView

---
+++ NMS-IOS/View/MyPage/CustomCollectionView.swift
@@ -0,0 +1,15 @@
+//
+//  CustomCollectionView.swift
+//  NMS-IOS
+//
+//  Created by 김대희 on 2021/11/22.
+//
+
+import Foundation
+import UIKit
+
+class CustomCollectionView: UICollectionView {
+
+    // none
+
+}
