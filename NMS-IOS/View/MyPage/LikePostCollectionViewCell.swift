d7203fc99c80281d2b07659eaf10b77bfa0982b8
김대희 <0824dh@naver.com>
2021-11-22 08:20:48 +0900

    📑 :: LikePostCollectionViewCell

---
+++ NMS-IOS/View/MyPage/LikePostCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  LikePostCollectionViewCell.swift
+//  NMS-IOS
+//
+//  Created by 김대희 on 2021/11/22.
+//
+
+import UIKit
+
+class LikePostCollectionViewCell: UICollectionViewCell {
+    
+}
