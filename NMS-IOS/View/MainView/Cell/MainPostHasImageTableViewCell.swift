af2b0035243ffff14edd607fcec6bc6a0568c002
김대희 <0824dh@naver.com>
2021-11-22 20:24:23 +0900

    📑 ::  MainPostHasImageTableViewCell

---
+++ NMS-IOS/View/MainView/Cell/MainPostHasImageTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  MainPostHasImageTableViewCell.swift
+//  NMS-IOS
+//
+//  Created by 김대희 on 2021/11/22.
+//
+
+import UIKit
+
+class MainPostHasImageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
