97ce7e8e6195eaa1413dc90f515d56e7fdd8c018
김대희 <0824dh@naver.com>
2021-11-29 23:14:09 +0900

    ⚡️ :: FirstChangPassWordViewController 완료

--- NMS-IOS/View/MyPage/ChangPassWord/FirstChangPassWordViewController.swift
+++ NMS-IOS/View/MyPage/ChangPassWord/FirstChangPassWordViewController.swift
@@ -21,14 +21,38 @@ class FirstChangPassWordViewController: UIViewController {
     let backNavigationBarView  = UIView().then {
         $0.backgroundColor = UIColor(named: "MainColor1")
     }
+    let nowPasswordField = UITextField().then {
+        $0.attributedPlaceholder = NSAttributedString(
+            string: " 현재 비밀번호",
+            attributes: [NSAttributedString.Key.foregroundColor: UIColor(named: "MainColor2")!]
+        )
+        $0.tintColor = UIColor(named: "MainColor1")
+        $0.backgroundColor = UIColor.clear
+    }
+    let nextButton = UIButton().then {
+        $0.backgroundColor = UIColor(named: "MainColor1")
+        $0.setTitle("다음", for: .normal)
+        $0.setTitleColor(.white, for: .normal)
+        $0.layer.cornerRadius = 25.5
+        $0.layer.borderColor = UIColor(named: "MainColor1")?.cgColor
+        $0.layer.borderWidth = 2
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         makeNavigationBar()
+        nowPasswordField.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
         view.addSubview(mainBackView)
         view.addSubview(backNavigationBarView)
+        mainBackView.addSubview(nowPasswordField)
+        mainBackView.addSubview(nextButton)
         makeConstraint()
         // Do any additional setup after loading the view.
+    }
+    override func viewDidLayoutSubviews() {
+        nowPasswordField.setUnderLine(color: UIColor(named: "MainColor2")!)
     }
     func makeNavigationBar() {
         self.view.backgroundColor = .systemBackground
@@ -51,5 +75,53 @@ class FirstChangPassWordViewController: UIViewController {
             $0.trailing.equalTo(0)
             $0.bottom.equalTo(self.view.safeAreaLayoutGuide.snp.top)
         }
+        nowPasswordField.snp.makeConstraints {
+            $0.width.equalTo(356)
+            $0.height.equalTo(35)
+            $0.top.equalTo(150)
+            $0.centerX.equalTo(self.view).offset(0)
+        }
+        nextButton.snp.makeConstraints {
+            $0.width.equalTo(356)
+            $0.height.equalTo(51)
+            $0.bottom.equalTo(-35)
+            $0.centerX.equalTo(self.view).offset(0)
+        }
     }
+}
+extension FirstChangPassWordViewController : UITextFieldDelegate {
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        textField.setUnderLine(color: UIColor(named: "MainColor1")!)
+        return true
+    }
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        textField.setUnderLine(color: UIColor(named: "MainColor2")!)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        self.view.endEditing(true)
+    }
+    @objc func keyboardWillShow(noti: Notification) {
+        let notinfo = noti.userInfo!
+        let keyboardFrame = notinfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        let heiget = -(keyboardFrame.size.height - self.view.safeAreaInsets.bottom + 20)
+        let animateDuration = notinfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+        UIView.animate(withDuration: animateDuration) {
+            self.nextButton.snp.updateConstraints() {
+                $0.bottom.equalTo(heiget)
+            }
+            self.view.layoutIfNeeded()
+        }
+    }
+    @objc func keyboardWillHide(noti: Notification) {
+        let notinfo = noti.userInfo!
+        let animateDuration = notinfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+        UIView.animate(withDuration: animateDuration) {
+            self.nextButton.snp.updateConstraints() {
+                $0.bottom.equalTo(-30)
+            }
+            self.view.layoutIfNeeded()
+        }
+    }
+    
 }
