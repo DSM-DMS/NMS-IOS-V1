65f3572232558eeb3d14fce228427c13a8079c7f
김대희 <0824dh@naver.com>
2021-12-01 21:50:25 +0900

    📑 ::  DetailPostViewController

---
+++ NMS-IOS/View/MainView/Cell/DetailPostViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailPostViewController.swift
+//  NMS-IOS
+//
+//  Created by 김대희 on 2021/12/01.
+//
+
+import UIKit
+
+class DetailPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+    
+}
