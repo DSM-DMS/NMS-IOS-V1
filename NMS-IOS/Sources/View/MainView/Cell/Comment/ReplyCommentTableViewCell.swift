30281b5e4b54919ce1d6db4617c7ec1b2fad3a01
김대희 <0824dh@naver.com>
2021-12-04 21:12:29 +0900

    📑 ::  ReplyCommentTableViewCell,  MainCommentTableViewCell

---
+++ NMS-IOS/Sources/View/MainView/Cell/Comment/ReplyCommentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ReplyCommentTableViewCell.swift
+//  NMS-IOS
+//
+//  Created by 김대희 on 2021/12/04.
+//
+
+import UIKit
+
+class ReplyCommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
