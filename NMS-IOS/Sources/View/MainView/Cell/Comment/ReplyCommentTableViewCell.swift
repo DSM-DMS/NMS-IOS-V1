b2fec74741829deabb1c8de5f741b2623d1ebd5a
김대희 <0824dh@naver.com>
2021-12-05 22:35:09 +0900

    ⚡️ :: comment tableViewCell 추가

--- NMS-IOS/Sources/View/MainView/Cell/Comment/ReplyCommentTableViewCell.swift
+++ NMS-IOS/Sources/View/MainView/Cell/Comment/ReplyCommentTableViewCell.swift
@@ -6,9 +6,72 @@
 //
 
 import UIKit
+import Then
+import SnapKit
+import RxSwift
+import RxCocoa
 
 class ReplyCommentTableViewCell: UITableViewCell {
 
+    var userImage = UIImageView().then {
+        $0.image = UIImage(named: "noImage")
+        $0.layer.borderWidth = 1
+        $0.clipsToBounds = true
+        $0.layer.borderColor = UIColor.clear.cgColor
+    }
+    var useridLabel = UILabel().then {
+        $0.text = "장성혜(마이스터부)"
+        $0.font = UIFont(name: "NotoSansKR-Bold", size: 14.0)
+    }
+    var commentLocationLabel = UILabel().then {
+        $0.text = "날짜가 들어가요"
+        $0.font = UIFont(name: "NotoSansKR-Medium", size: 10.0)
+        $0.textColor = .secondaryLabel
+    }
+    var commentLabel = UILabel().then {
+        $0.text = " 제목이 들어가요"
+        $0.font = UIFont(name: "NotoSansKR-Medium", size: 12.0)
+    }
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super .init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemBackground
+        contentView.addSubview(userImage)
+        userImage.layer.cornerRadius = 18
+        contentView.addSubview(useridLabel)
+        contentView.addSubview(commentLocationLabel)
+        contentView.addSubview(commentLabel)
+        userImage.snp.makeConstraints {
+            $0.width.equalTo(36)
+            $0.height.equalTo(36)
+            $0.top.equalTo(20)
+            $0.left.equalTo(40)
+        }
+        useridLabel.snp.makeConstraints {
+//            $0.width.equalTo(150)
+            $0.height.equalTo(20)
+            $0.top.equalTo(20)
+            $0.left.equalTo(self.userImage).offset(45)
+        }
+        commentLocationLabel.snp.makeConstraints {
+            $0.width.equalTo(150)
+            $0.height.equalTo(18)
+            $0.top.equalTo(40)
+            $0.bottom.equalTo(-20)
+            $0.left.equalTo(self.userImage).offset(45)
+        }
+        commentLabel.snp.makeConstraints {
+            $0.top.equalTo(20)
+            $0.height.equalTo(20)
+            $0.left.equalTo(self.useridLabel.snp.right).offset(5)
+        }
+
+    }
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- NMS-IOS/Sources/View/MainView/DetailPostViewController.swift
+++ NMS-IOS/Sources/View/MainView/DetailPostViewController.swift
@@ -18,6 +18,7 @@ class DetailPostViewController: UIViewController {
     let mainVC = MainViewController()
     let bag = DisposeBag()
     let store = MainPost()
+    let comment = MainComment()
     
     let mainBackView = UIView().then {
         $0.backgroundColor = .systemBackground
@@ -35,9 +36,11 @@ class DetailPostViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         makeNavigationBar()
+        mainTableView.register(MainCommentTableViewCell.self, forCellReuseIdentifier: "cell")
+        mainTableView.register(ReplyCommentTableViewCell.self, forCellReuseIdentifier: "cell1")
         mainTableView.register(MainPostTableViewCell.self, forCellReuseIdentifier: "cell2")
         mainTableView.register(MainPostHasImageTableViewCell.self, forCellReuseIdentifier: "cell3")
-
+        
         view.addSubview(mainBackView)
         view.addSubview(backNavigationBarView)
         mainBackView.addSubview(mainTableView)
@@ -54,8 +57,6 @@ class DetailPostViewController: UIViewController {
     func makeNavigationBar() {
         self.view.backgroundColor = .systemBackground
         self.navigationController?.navigationBar.tintColor = .white
-//        self.navigationController?.navigationBar.barTintColor =  UIColor(named: "MainColor1")
-
         self.navigationController?.navigationBar.layoutIfNeeded()
         self.navigationItem.title =  "\(store.list[indexNum].Title)"
         self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
@@ -84,61 +85,78 @@ class DetailPostViewController: UIViewController {
 extension DetailPostViewController : UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 1 + comment.list.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let bgColorView = UIView()
         bgColorView.backgroundColor = .clear
-        if store.list[indexNum].PostImage == nil {
-            let Pcell = tableView.dequeueReusableCell(withIdentifier: "cell2") as! MainPostTableViewCell
-            
-            Pcell.reportButtonAction = { [unowned self] in
-                AudioServicesPlaySystemSound(1520)
-                Pcell.likeButton.isSelected.toggle()
-                Pcell.likeButton.isSelected = revertBool(bool: Pcell.likeButton.isSelected)
+        if indexPath.row == 0 {
+            if store.list[indexNum].PostImage == nil {
+                let Pcell = tableView.dequeueReusableCell(withIdentifier: "cell2") as! MainPostTableViewCell
+                
+                Pcell.reportButtonAction = { [unowned self] in
+                    AudioServicesPlaySystemSound(1520)
+                    Pcell.likeButton.isSelected.toggle()
+                    Pcell.likeButton.isSelected = revertBool(bool: Pcell.likeButton.isSelected)
+                }
+                Pcell.postTitleTextView.text = "\(store.list[indexNum].Title)"
+                Pcell.postLocationLabel.text = "\(store.list[indexNum].LocationDate)"
+                Pcell.mainPostTextView.text = "\(store.list[indexNum].Body)"
+                Pcell.likeCountLabel.setTitle(" \(store.list[indexNum].LikeCount)", for: .normal)
+                Pcell.commentCountLabel.text = "댓글 \(store.list[indexNum].CommentCount)"
+                if store.list[indexNum].badge?.count == 1 {
+                    badgeSetting(title: "\(store.list[indexNum].badge![0])", target: Pcell.categorybadge)
+                } else if store.list[indexNum].badge?.count == 2 {
+                    badgeSetting(title: "\(store.list[indexNum].badge![0])", target: Pcell.categorybadge)
+                    badgeSetting(title: "\(store.list[indexNum].badge![1])", target: Pcell.categorybadge2)
+                }
+                Pcell.selectedBackgroundView = bgColorView
+                return Pcell
             }
-            Pcell.postTitleTextView.text = "\(store.list[indexNum].Title)"
-            Pcell.postLocationLabel.text = "\(store.list[indexNum].LocationDate)"
-            Pcell.mainPostTextView.text = "\(store.list[indexNum].Body)"
-            Pcell.likeCountLabel.setTitle(" \(store.list[indexNum].LikeCount)", for: .normal)
-            Pcell.commentCountLabel.text = "댓글 \(store.list[indexNum].CommentCount)"
-            if store.list[indexNum].badge?.count == 1 {
-                badgeSetting(title: "\(store.list[indexNum].badge![0])", target: Pcell.categorybadge)
-            } else if store.list[indexNum].badge?.count == 2 {
-                badgeSetting(title: "\(store.list[indexNum].badge![0])", target: Pcell.categorybadge)
-                badgeSetting(title: "\(store.list[indexNum].badge![1])", target: Pcell.categorybadge2)
+            else  {
+                let Hcell = tableView.dequeueReusableCell(withIdentifier: "cell3") as! MainPostHasImageTableViewCell
+                
+                Hcell.reportButtonAction = { [unowned self] in
+                    AudioServicesPlaySystemSound(1520)
+                    Hcell.likeButton.isSelected.toggle()
+                    Hcell.likeButton.isSelected = revertBool(bool: Hcell.likeButton.isSelected)
+                }
+                Hcell.postTitleTextView.text = "\(store.list[indexNum ].Title)"
+                Hcell.postLocationLabel.text = "\(store.list[indexNum].LocationDate)"
+                Hcell.mainPostTextView.text = "\(store.list[indexNum].Body)"
+                Hcell.likeCountLabel.setTitle(" \(store.list[indexNum].LikeCount)", for: .normal)
+                Hcell.commentCountLabel.text = "댓글 \(store.list[indexNum].CommentCount)"
+                if store.list[indexNum].badge?.count == 1 {
+                    badgeSetting(title: "\(store.list[indexNum].badge![0])", target: Hcell.categorybadge)
+                } else if store.list[indexNum].badge?.count == 2 {
+                    badgeSetting(title: "\(store.list[indexNum].badge![0])", target: Hcell.categorybadge)
+                    badgeSetting(title: "\(store.list[indexNum].badge![1])", target: Hcell.categorybadge2)
+                }
+                Hcell.PostImage.image = store.list[indexNum].PostImage
+                Hcell.selectedBackgroundView = bgColorView
+                return Hcell
             }
-            Pcell.selectedBackgroundView = bgColorView
-            return Pcell
         }
         else {
-            let Hcell = tableView.dequeueReusableCell(withIdentifier: "cell3") as! MainPostHasImageTableViewCell
-            
-            Hcell.reportButtonAction = { [unowned self] in
-                AudioServicesPlaySystemSound(1520)
-                Hcell.likeButton.isSelected.toggle()
-                Hcell.likeButton.isSelected = revertBool(bool: Hcell.likeButton.isSelected)
+            if comment.list[indexPath.row - 1].commentHashtagBool == false {
+                let Ccell = tableView.dequeueReusableCell(withIdentifier: "cell") as! MainCommentTableViewCell
+                Ccell.userImage.image = comment.list[indexPath.row - 1].userImage ?? UIImage(named: "noImage")
+                Ccell.useridLabel.text = comment.list[indexPath.row - 1].userName
+                Ccell.commentLocationLabel.text = comment.list[indexPath.row - 1].locationDate
+                Ccell.commentLabel.text = comment.list[indexPath.row - 1].commentBody
+                
+                return Ccell
+            } else {
+                let CBcell = tableView.dequeueReusableCell(withIdentifier: "cell1") as! ReplyCommentTableViewCell
+                CBcell.userImage.image = comment.list[indexPath.row - 1].userImage ?? UIImage(named: "noImage")
+                CBcell.useridLabel.text = comment.list[indexPath.row - 1].userName
+                CBcell.commentLocationLabel.text = comment.list[indexPath.row - 1].locationDate
+                CBcell.commentLabel.text = comment.list[indexPath.row - 1].commentBody
+                return CBcell
             }
-            Hcell.postTitleTextView.text = "\(store.list[indexNum ].Title)"
-            Hcell.postLocationLabel.text = "\(store.list[indexNum].LocationDate)"
-            Hcell.mainPostTextView.text = "\(store.list[indexNum].Body)"
-            Hcell.likeCountLabel.setTitle(" \(store.list[indexNum].LikeCount)", for: .normal)
-            Hcell.commentCountLabel.text = "댓글 \(store.list[indexNum].CommentCount)"
-            if store.list[indexNum].badge?.count == 1 {
-                badgeSetting(title: "\(store.list[indexNum].badge![0])", target: Hcell.categorybadge)
-            } else if store.list[indexNum].badge?.count == 2 {
-                badgeSetting(title: "\(store.list[indexNum].badge![0])", target: Hcell.categorybadge)
-                badgeSetting(title: "\(store.list[indexNum].badge![1])", target: Hcell.categorybadge2)
-            }
-            
-            Hcell.PostImage.image = store.list[indexNum].PostImage
-            Hcell.selectedBackgroundView = bgColorView
-            return Hcell
-            
         }
-        
     }
     func revertBool(bool : Bool) -> Bool {
         if bool == false {
