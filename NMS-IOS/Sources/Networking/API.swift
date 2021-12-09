f8ed8758142a202896f36e4a84d4d89c7989ea75
김대희 <0824dh@naver.com>
2021-12-09 23:11:22 +0900

    ⚡️ :: API Case 추가

--- NMS-IOS/Sources/Networking/API.swift
+++ NMS-IOS/Sources/Networking/API.swift
@@ -14,7 +14,21 @@ enum API {
     case signUo
     case reissiuTokens  // 토큰 재발급
     case reviseStudent  // 학생 정보 수정
-    
     case myPage
+    
+    case checkEmail
+    case sendEmail
+    
+    case checkAllPost
+    case checkTargetPost( _ target : String)
+    case checkJustPost( _ noticeId : String)
+    
+    case bookMark( _ noticeId : String)
+    case cancelBookMark( _ noticeId : String)
+    
+    case writeComment( _ noticeId : String)
+    case writeReComment( _ noticeId : String)
+    
+    
     
 }
